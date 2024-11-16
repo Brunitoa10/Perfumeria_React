7a5ed87f6c801ef32c06db3d762771b43b9db101
Bruno A Parisi <bnoprs00@gmail.com>
2024-11-16 10:30:49 -0300

    Agregue mas perfumes, efecto carrusel pero no lo aplique aun

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -19,6 +19,22 @@ const products = [
     imageUrl: 'https://imgs.search.brave.com/WUCvGGQ29yGyCW6WC57S4nV0QKIGNF7bc__jQv0O-eU/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NjEyRERWRUhMbUwu/anBn',
     description: 'Woody aquatic fragrance',
     details: 'Invictus by Paco Rabanne is a vibrant and energetic masculine fragrance launched in 2013. It represents the spirit of the champion, combining fresh and aquatic notes with a sensual and woody base. Its main accords include grapefruit, bay leaves and ambergris, creating a modern aroma that balances freshness and warmth. It is ideal for men looking to convey confidence and dynamism.'
+  },  {
+    id: 100,
+    name: 'Pure XS',
+    category: 'Perfume for Her',
+    price: 850,
+    imageUrl: 'https://imgs.search.brave.com/Tctywei0TC8WBxCXHKTD0717oh0Kg1toyi9Ux5s84QE/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly93d3cu/ZmFybWFjaWFsZWxv/aXIuY29tLmFyL2lt/Zy9hcnRpY3Vsb3Mv/cGFjb19yYWJhbm5l/X3B1cmVfeHNfZm9y/X2hlcl9lYXVfZGFf/cGFyZnVtX211amVy/X2ltYWdlbjEuanBn',
+    description: 'oriental floral fragrance',
+    details: 'Pure XS for Her by Paco Rabanne is an intense and seductive feminine fragrance, launched in 2018. It belongs to the oriental floral family, standing out for its daring combination of ylang-ylang, vanilla and popcorn. It is a provocative, sweet and enveloping perfume, designed for women who radiate sensuality and confidence. Perfect for special occasions or elegant evenings.'
+  },{
+    id: 101,
+    name: 'Yellow Diamond Intense',
+    category: 'Perfume for Her',
+    price: 750,
+    imageUrl: 'https://imgs.search.brave.com/nhBtYw7RIoSh7AJxdaY_9dOh9pktAWRx6bWo7CNJ8YE/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS52b2d1ZS5teC9w/aG90b3MvNjJlM2Zk/ZDM3MjY3MDg2NTI2/MDdiNzFhL21hc3Rl/ci93XzE2MDAsY19s/aW1pdC9QZXJmdW1l/LVllbGxvdy1EaWFt/b25kLUludGVuc2Ut/RGUtVmVyc2FjZS1Q/YXJhLU11amVyLmpw/Zw',
+    description: 'Woody aquatic fragrance',
+    details: 'Versace Yellow Diamond Intense is a luminous and sophisticated feminine fragrance, launched in 2014. It belongs to the floral family and combines fresh notes of citrus, elegant flowers such as jasmine and freesia, with a warm base of amber and musk. It is a vibrant and long-lasting perfume, ideal for elegant women looking to radiate energy and freshness on any occasion.'
   },
   // Más productos
 ];
