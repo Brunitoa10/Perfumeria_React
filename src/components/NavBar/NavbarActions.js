74b7074d1d254d97c9c36ee2c0eb917bd7dafd8c
Bruno A Parisi <bnoprs00@gmail.com>
2024-11-16 09:39:11 -0300

    Agregue oyente carrito compras

--- src/components/NavBar/NavbarActions.js
+++ src/components/NavBar/NavbarActions.js
@@ -1,16 +1,21 @@
 import ShoppingCart from '@mui/icons-material/ShoppingCart';
-import Button from '@mui/material/Button';
+import { Box, Button, IconButton } from '@mui/material';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './NavbarStyles';
 
-const NavbarActions = () => (
-  <>
-    <Button sx={styles.navbarButton} component={Link} to="/login">
-      <strong>Sign In</strong>
-    </Button>
-    <ShoppingCart />
-  </>
-);
+const NavbarActions = () => {
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Button sx={styles.navbarButton} component={Link} to="/login" aria-label="Sign in">
+        <strong>Sign In</strong>
+      </Button>
+
+      <IconButton sx={styles.navbarButton} component={Link} to="/shopping" aria-label="Shopping cart">
+        <ShoppingCart />
+      </IconButton>
+    </Box>
+  );
+};
 
 export default NavbarActions;
